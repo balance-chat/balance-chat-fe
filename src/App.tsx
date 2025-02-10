ccbcd70da10452c98506086df0da96e66129e0d6
Eunji <eeunjiilee@gmail.com>
2025-02-10 22:08:34 +0900

    feat: add RootContainer component for center alignment

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './components/login/Login';
 import Join from './components/join/Join';
 import Chat from './components/chat/Chat';
+import { RootContainer } from './components/ui/Layout';
 
 function App() {
   return (
-    <div>
+    <RootContainer>
       <Routes>
         <Route path="/" />
         <Route path="/login" element={<Login />} />
         <Route path="/join" element={<Join />} />
         <Route path="/chat" element={<Chat />} />
       </Routes>
-    </div>
+    </RootContainer>
   );
 }
 
