46714fc18e3d475cedd694e5315c4b0c885c67a9
Eunji <eeunjiilee@gmail.com>
2025-02-10 09:18:56 +0900

    feat: add router pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/login/Login';
+import Join from './components/join/Join';
+import Chat from './components/chat/Chat';
+
 function App() {
-  return <div>밸런스 채팅</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" />
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
