7c71a3165099c264a3d252669fa05225e6d46e42
Eunji <eeunjiilee@gmail.com>
2025-02-12 19:39:17 +0900

    feat: add user and other messages components

---
+++ src/components/chat/Messages/UserMessages.tsx
@@ -0,0 +1,22 @@
+/** @jsxImportSource @emotion/react */
+
+import { ChatBy } from '@/types/message.type';
+import ChatBubble from '../ChatBubble/ChatBubble';
+import { container } from './UserMessages.css';
+
+interface UserMessagesProps {
+  by: ChatBy.User;
+  messages: string[];
+}
+
+function UserMessages({ by, messages }: UserMessagesProps) {
+  return (
+    <div css={[container]}>
+      {messages.map((msg, i) => (
+        <ChatBubble key={i} message={msg} by={by} />
+      ))}
+    </div>
+  );
+}
+
+export default UserMessages;
