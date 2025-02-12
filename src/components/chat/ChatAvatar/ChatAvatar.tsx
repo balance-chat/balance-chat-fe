c12672e181f412cd968c5bf72b3d57219b3f2fa3
Eunji Lee <77034159+eunjios@users.noreply.github.com>
2025-02-12 19:37:12 +0900

    feat: add ChatAvatar component (#9)

---
+++ src/components/chat/ChatAvatar/ChatAvatar.tsx
@@ -0,0 +1,15 @@
+import { Avatar } from '@/components/ui/Avatar';
+
+interface Props {
+  picture?: string;
+}
+
+function ChatAvatar({ picture }: Props) {
+  return (
+    <div>
+      <Avatar picture={picture} />
+    </div>
+  );
+}
+
+export default ChatAvatar;
