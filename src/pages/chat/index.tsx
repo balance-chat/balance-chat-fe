fa45c0353401eb02445c6aeab6f5bcd7bccf04eb
Eunji <eeunjiilee@gmail.com>
2025-02-12 19:44:17 +0900

    feat: display message groups using dummy data

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,5 +1,24 @@
-import { Chat } from '../../components/ui/Layout';
-import ChatInput from '../../components/chat/ChatInput/ChatInput';
+import { ChatBy } from '@/types/message.type';
+import { Chat } from '@/components/ui/Layout';
+import ChatInput from '@/components/chat/ChatInput/ChatInput';
+import MessageGroup from '@/components/chat/MessageGroup/MessageGroup';
+
+const TEST_MESSAGE = {
+  MESSAGES: [
+    '긴 텍스트 메시지 테스트 긴 텍스트 메시지 테스트긴 텍스트 메시지 테스트긴 텍스트 메시지 테스트',
+  ],
+};
+const TEST_MESSAGE_1 = {
+  USER_NAME: '프로필 사진 없는 유저',
+  MESSAGES: ['그룹 메시지 테스트 1', '그룹 메시지 테스트 2'],
+};
+
+const TEST_MESSAGE_2 = {
+  USER_NAME: '프로필 사진 있는 유저',
+  MESSAGES: ['단독 메시지 테스트'],
+  PICTURE:
+    'https://i.pinimg.com/736x/d5/4d/d2/d54dd20779e0abab679de056ff313411.jpg',
+};
 
 function ChatPage() {
   return (
@@ -8,9 +27,18 @@ function ChatPage() {
         <div>option 1 vs option 2</div>
       </Chat.Section>
       <Chat.Content>
-        <div>메시지</div>
-        <div>메시지</div>
-        <div>메시지</div>
+        <MessageGroup by={ChatBy.User} messages={TEST_MESSAGE.MESSAGES} />
+        <MessageGroup
+          by={ChatBy.Other}
+          username={TEST_MESSAGE_1.USER_NAME}
+          messages={TEST_MESSAGE_1.MESSAGES}
+        />
+        <MessageGroup
+          by={ChatBy.Other}
+          username={TEST_MESSAGE_2.USER_NAME}
+          picture={TEST_MESSAGE_2.PICTURE}
+          messages={TEST_MESSAGE_2.MESSAGES}
+        />
       </Chat.Content>
       <Chat.Section>
         <ChatInput />
