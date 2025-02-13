e1c18fb5d3586a8fbc98d7a9734724518795dba0
Eunji <eeunjiilee@gmail.com>
2025-02-13 22:50:46 +0900

    feat: replace Join with JoinPage

---
+++ src/pages/join/index.tsx
@@ -0,0 +1,12 @@
+import Join from '@/components/join/Join/Join';
+import { Center } from '@/components/ui/Layout';
+
+function JoinPage() {
+  return (
+    <Center>
+      <Join />
+    </Center>
+  );
+}
+
+export default JoinPage;
