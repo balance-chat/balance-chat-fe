4230de888c33129ba5c6422d3595526ba8e1a10d
Eunji <eeunjiilee@gmail.com>
2025-02-14 00:05:35 +0900

    feat: add login page component

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,12 @@
+import Login from '@/components/login/Login/Login';
+import { Center } from '@/components/ui/Layout';
+
+function LoginPage() {
+  return (
+    <Center>
+      <Login />
+    </Center>
+  );
+}
+
+export default LoginPage;
