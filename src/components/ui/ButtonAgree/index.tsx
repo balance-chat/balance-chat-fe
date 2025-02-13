5badda16250c8515e73a0f21eaa0c32c948237a0
Eunji <eeunjiilee@gmail.com>
2025-02-13 22:11:57 +0900

    feat: add agree button component

---
+++ src/components/ui/ButtonAgree/index.tsx
@@ -0,0 +1,33 @@
+/** @jsxImportSource @emotion/react */
+
+import { BiCheck } from 'react-icons/bi';
+import { btn, checked, icon, txt, unchecked } from './index.css';
+import { useState } from 'react';
+
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+function ButtonAgree({ children, ...props }: Props) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const toggle = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    e.preventDefault();
+    setIsChecked((prev) => !prev);
+  };
+
+  return (
+    <button
+      css={[btn, isChecked ? checked : unchecked]}
+      onClick={toggle}
+      {...props}
+    >
+      <div css={icon}>
+        <BiCheck />
+      </div>
+      <div css={txt}>{children}</div>
+    </button>
+  );
+}
+
+export { ButtonAgree };
