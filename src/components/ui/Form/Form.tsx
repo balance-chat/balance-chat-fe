1f3f4681acbcadd87e1ca7174b8cf58fccb4d02e
Eunji <eeunjiilee@gmail.com>
2025-02-14 18:22:07 +0900

    feat: Form props extends html form attributes

--- src/components/ui/Form/Form.tsx
+++ src/components/ui/Form/Form.tsx
@@ -2,17 +2,24 @@
 
 import { ButtonAgree } from '../ButtonAgree/ButtonAgree';
 import { ButtonSubmit } from '../ButtonSubmit/ButtonSubmit';
+import { EmailField } from '../Field/Email/EmailField';
+import { PasswordField } from '../Field/Password/PasswordField';
 import { FormSection } from '../FormSection/FormSection';
 import { Input } from '../Input/Input';
+import { InputError } from '../InputError/InputError';
 import { InputWithCheckBtn } from '../InputWithCheckBtn/InputWithCheckBtn';
 import { container } from './Form.css';
 
-interface Props {
+interface Props extends React.HTMLAttributes<HTMLFormElement> {
   children: React.ReactNode;
 }
 
-function Form({ children }: Props) {
-  return <form css={container}>{children}</form>;
+function Form({ children, ...props }: Props) {
+  return (
+    <form css={container} {...props}>
+      {children}
+    </form>
+  );
 }
 
 Form.Section = FormSection;
@@ -20,5 +27,8 @@ Form.Input = Input;
 Form.InputWithCheckBtn = InputWithCheckBtn;
 Form.Agree = ButtonAgree;
 Form.Submit = ButtonSubmit;
+Form.ErrorMessage = InputError;
+Form.EmailField = EmailField;
+Form.PasswordField = PasswordField;
 
 export { Form };
