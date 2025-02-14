58d12ac1d8719075f5cdb38cfabc74b8111b105e
Eunji <eeunjiilee@gmail.com>
2025-02-14 18:22:51 +0900

    feat: validate login form data

--- src/components/features/login/LoginForm/LoginForm.tsx
+++ src/components/features/login/LoginForm/LoginForm.tsx
@@ -1,24 +1,35 @@
 /** @jsxImportSource @emotion/react */
 
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
+import { LoginFormData } from '@/types/auth/account';
 import { Form } from '@/components/ui/Form/Form';
 import { forgot } from './LoginForm.css';
 
 function LoginForm() {
+  const methods = useForm<LoginFormData>({
+    mode: 'onTouched',
+  });
+
+  const onSubmit: SubmitHandler<LoginFormData> = (data) => {
+    // @todo: submit login form data
+    console.log(data);
+  };
+
   return (
-    <Form>
-      <Form.Section>
-        <Form.Input type="email" placeholder="이메일 주소" isValid={true} />
-      </Form.Section>
-      <Form.Section>
-        <Form.Input type="password" placeholder="비밀번호" isValid={true} />
+    <FormProvider {...methods}>
+      <Form onSubmit={methods.handleSubmit(onSubmit)}>
+        <Form.EmailField />
+        <Form.PasswordField />
         <div css={forgot}>
           <a>비밀번호를 잊어버렸나요?</a>
         </div>
-      </Form.Section>
-      <Form.Section>
-        <Form.Submit isValid={true}>로그인하기</Form.Submit>
-      </Form.Section>
-    </Form>
+        <Form.Section>
+          <Form.Submit isValid={methods.formState.isValid}>
+            로그인하기
+          </Form.Submit>
+        </Form.Section>
+      </Form>
+    </FormProvider>
   );
 }
 
