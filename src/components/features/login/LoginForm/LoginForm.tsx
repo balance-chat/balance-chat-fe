7fa76dd39561e3b4ca89ff7bc0209f09dcb5ca26
Eunji <eeunjiilee@gmail.com>
2025-02-16 13:20:24 +0900

    refactor: replace login form fields with FormField

--- src/components/features/login/LoginForm/LoginForm.tsx
+++ src/components/features/login/LoginForm/LoginForm.tsx
@@ -6,6 +6,7 @@ import { Form } from '@/components/ui/Form/Form';
 import { forgot } from './LoginForm.css';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { loginSchema } from '@/constants/yup';
+import { loginField } from '@/constants/form/login';
 
 function LoginForm() {
   const methods = useForm<LoginFormData>({
@@ -21,11 +22,18 @@ function LoginForm() {
   return (
     <FormProvider {...methods}>
       <Form onSubmit={methods.handleSubmit(onSubmit)}>
-        <Form.EmailField />
-        <Form.PasswordField />
+        <Form.Section>
+          <Form.Field field={loginField.email} />
+        </Form.Section>
+
+        <Form.Section>
+          <Form.Field field={loginField.password} />
+        </Form.Section>
+
         <div css={forgot}>
           <a>비밀번호를 잊어버렸나요?</a>
         </div>
+
         <Form.Section>
           <Form.Submit isValid={methods.formState.isValid}>
             로그인하기
